8d403985d1323b97f5f99f641d6bec86cb920e3a
Anton Vikharev <anton.vikharev@decentraland.org>
2023-03-02 22:35:33 +0300

    external logging implemented

---
+++ Core Modules/WalletConnectSharp2.Common/Logging/IWC2Logger.cs
@@ -0,0 +1,8 @@
+﻿namespace WalletConnectSharp.Common.Logging;
+
+public interface IWC2Logger
+{
+	void Log(string text);
+	void LogWarning(string text);
+	void LogError(string text);
+}
---
+++ Core Modules/WalletConnectSharp2.Common/Logging/WC2Debug.cs
@@ -0,0 +1,26 @@
+﻿namespace WalletConnectSharp.Common.Logging;
+
+public static class WC2Debug
+{
+	private static readonly List<IWC2Logger> Loggers = new List<IWC2Logger>();
+	
+	public static void AddLogger(IWC2Logger logger)
+	{
+		Loggers.Add(logger);
+	}
+	
+	public static void Log(string text)
+	{
+		Loggers.ForEach(l => l.Log(text));
+	}
+	
+	public static void LogWarning(string text)
+	{
+		Loggers.ForEach(l => l.LogWarning(text));
+	}
+	
+	public static void LogError(string text)
+	{
+		Loggers.ForEach(l => l.LogWarning(text));
+	}
+}
